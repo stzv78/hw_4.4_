4dc989643716ef659c15bd128af0bf83ac1fbd02
stzv78 <stzv78@mail.ru>
2017-07-27 00:16:32 +0300

    second

---
+++ manager.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'classes/DataBase.php';
+session_start();
+
+//авторизованный пользователь
+$user = $_SESSION['login'];
+$user_id = $_SESSION['user_id'];
+
+$objDb = new DataBase;
+$action = "";
+
+if (isset($_POST['action'])) {
+	$action = isset($_POST['action']) ? 'create': "";
+    $name = $_POST['name'];
+	} else {
+        $_GET['name'] = isset($_GET['name']) ? filter_input(INPUT_GET, 'name', FILTER_SANITIZE_SPECIAL_CHARS) : '';
+        $_GET['field'] = isset($_GET['field']) ? filter_input(INPUT_GET, 'field', FILTER_SANITIZE_SPECIAL_CHARS) : '';
+        $_GET['type'] = isset($_GET['type']) ? filter_input(INPUT_GET, 'type', FILTER_SANITIZE_SPECIAL_CHARS) : 'NULL';
+        $_GET['rename'] = isset($_GET['rename']) ? filter_input(INPUT_GET, 'rename', FILTER_SANITIZE_SPECIAL_CHARS) : 'NULL';
+        $_GET['newType'] = isset($_GET['newType']) ? filter_input(INPUT_GET, 'newType', FILTER_SANITIZE_SPECIAL_CHARS) : 'NULL';
+        $action = isset($_GET['action']) ? filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS) : '';
+    }
+    
+switch ($action) {
+    case 'create':
+        //создаем таблицу 
+        if (!ctype_space($name)){
+            echo $action . $name;
+            $objDb->createTable($name);
+            } else {
+            echo "<p style='color: red;'>Некорретный ввод имени таблицы!</p>";
+        }
+        break;
+    case 'dropColumn':
+        //удалить стоблец
+        $objDb->dropColumn($_GET['name'], $_GET['field']);
+        break;
+    case 'editColumnType':
+        //изменяем тип поля
+        $objDb->editColumnType($_GET['name'], $_GET['field'], $_GET['newType']);
+        break;
+    case 'renameColumn':
+        //переименовываем столбец
+        $objDb->renameColumn($_GET['name'], $_GET['field'], $_GET['rename'], $_GET['type']);
+        break;
+}
+
+//получаем список всех таблиц в БД
+$tableList = $objDb->showTables();
+
+//выводим список таблиц
+if ($tableList) {
+    include_once "tmpl.php";
+    } else {
+    echo '<h4>Нет таблиц в базе данных " '. $objDb->dbname . '":</h4>';
+}
+?>
---
+++ tmpl.php
@@ -0,0 +1,102 @@
+<?php
+$types = array(
+        'tinyint(4)',
+        'smallint(2)',
+        'mediumint(3)',
+        'int(9)',
+        'bigint(11)',
+        'float',
+        'double',
+        'char(256)',
+        'varchar',
+        'tinytext',
+        'text',
+   );
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+<h4>Список таблиц базы данных: <?= $objDb->dbname ?></h4>
+<ul>
+    <?php foreach ($tableList as $value): ?>
+    <li><a href="?name=<?php echo $value ?>"><?= $value ?></a></li>
+     <?= $_GET['acction'] ?>
+
+        <?php if (isset($_GET['name']) && ($_GET['name'] === $value)): ?>
+            <table border ='1'>
+                <thead>
+                   <tr>
+                        <th>Имя поля</th>
+                        <th>Тип поля</th>
+                        <th>Удалить</th>
+                    </tr>
+                </thead>
+            
+            <?php foreach ($objDb->infoTable($value) as $fields): ?>
+                <tr>
+                    <td  align="right" width="250">
+                   
+                    <?php if (($_GET['action'] === 'editName') && ($_GET['field'] === $fields['Field'])): ?>
+                        <form method="GET" action="manager.php" >
+                            <input type="text" name="rename" value="<?= $fields['Field'] ?>">
+                            <input type="hidden" name="name" value="<?= $value ?>">
+                            <input type="hidden" name="field" value="<?= $fields['Field'] ?>">
+                            <input type="hidden" name="type" value="<?= $fields['Type'] ?>">
+                            <button style="vertical-align: middle" type="submit" name="action" value="renameColumn">
+                            <img  src='images/ok.png' width='16' height='16' alt='Изменить'>
+                            </button>
+                            <button style="vertical-align: middle"  type="cancel" name="action" value="">
+                            <img valign='center' src='images/cancel.png' width='16' height='16' alt='Отменить'>
+                            </button>
+                        </form>
+                    <?php else: ?>
+                        <?=$fields['Field']?>
+                        <a href="?name=<?= $value ?>&field=<?= $fields['Field']?>&action=editName">
+                        <img src='images/edit.png' width='16' height='16' alt='Изменить'></a>
+                    <?php endif; ?>
+                
+                    </td>
+
+                    <td align='right' width='150'>
+                    <?php if (($_GET['action'] === 'editType') && ($_GET['field'] === $fields['Field'])): ?>
+                        <form method="GET" action="manager.php">
+                            <select name="newType">
+                            <?php foreach ($types as $val): ?>
+                            <option value="<?= $val ?>"><?= $val ?></option>
+                               <?php endforeach; ?>
+                            </select>
+                            <input type="hidden" name="name" value="<?= $value ?>">
+                            <input type="hidden" name="field" value="<?= $fields['Field'] ?>">
+                            <input type="hidden" name="type" value="<?= $fields['Type'] ?>">
+                            <button style="vertical-align: middle" type="submit" name="action" value="editColumnType">
+                            <img  src='images/ok.png' width='16' height='16' alt='Изменить'>
+                            </button>
+                        </form>
+                    <?php else: ?>
+                        <?= $fields['Type'] ?>
+                        <a href="?name=<?= $value ?>&field=<?= $fields['Field'] ?>&action=editType"><img src='images/edit.png' width='16' height='16' alt='Изменить'></a>
+                    </td>
+                    <?php endif; ?>
+                    
+                    <td align='center'>
+                        <a href="?name=<?= $value ?>&field=<?= $fields['Field'] ?>&action=dropColumn">
+                        <img src='images/delete.png' width='16' height='16' alt='Удалить поле'></a>
+                    </td>
+
+                </tr>
+            <?php endforeach; ?>
+            </table>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</ul>
+<form method="POST" action="manager.php" >
+    <label>Новая таблица:</label>
+    <input type="text" name="name" value="" placeholder="Имя таблицы">
+    <input type="submit" name="action" value="Создать">
+</form>
+</body>
+</html>    
